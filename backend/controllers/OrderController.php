6e8967c5f0f07ad168a8e5dd1ceb9d9e1655d888
habiburrahman <remorac.14@gmail.com>
2023-03-09 16:03:06 +0700

    wip

--- backend/controllers/OrderController.php
+++ backend/controllers/OrderController.php
@@ -161,7 +161,7 @@ class OrderController extends Controller
     public function actionSet($schedule_id, $menu_id)
     {
         if (!Yii::$app->request->post('location_id')) {
-            Yii::$app->session->addFlash('error', 'Gagal menyimpan pemesanan. <b>Lokasi Pengantaran</b> harus diisi.');
+            Yii::$app->session->addFlash('error', 'Gagal menyimpan pemesanan. <br><b>Lokasi Pengantaran</b> harus diisi.');
             return $this->redirect(['index']);
         }
 
